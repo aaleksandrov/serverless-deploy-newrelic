8ba79673960cf9413c87cdc989270625f375f4ab
Alexey Alexandrov <alexey.aleksandrov@deliveryhero.com>
2018-07-11 13:13:06 +0200

    Debug

--- index.js
+++ index.js
@@ -30,6 +30,10 @@ class DeployNewRelicPlugin {
       }
     };
     this.serverless.cli.log('Recording new deployment');
+    this.serverless.cli.log('App id ');
+    this.serverless.cli.log(this.applicationId);
+    this.serverless.cli.log('adminApiKey');
+    this.serverless.cli.log(this.adminApiKey);
     const postData = JSON.stringify({
       deployment : {
         revision: 'blah',
